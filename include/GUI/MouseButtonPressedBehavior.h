093342ad80c79ea453172892aae85fb3ca8ad954
Bezifabr <bezifabr@gmail.com>
2020-05-05 19:42:52 +0200

    [DPS-62] Implement MouseButtonPressedBehavior

---
+++ include/GUI/MouseButtonPressedBehavior.h
@@ -0,0 +1,19 @@
+#ifndef GUI_MOUSE_BUTTON_PRESSED_BEHAVIOR_H
+#define GUI_MOUSE_BUTTON_PRESSED_BEHAVIOR_H
+
+#include "GUI/EventBehavior.h"
+#include "GUI/UIObject.h"
+#include <SFML/Window/Event.hpp>
+#include <memory>
+
+namespace gui {
+
+	class MouseButtonPressedBehavior : public gui::EventBehavior {
+	public:
+		MouseButtonPressedBehavior(std::shared_ptr<gui::UIObject> guiObject);
+		virtual void OnHandleEvent(const sf::Event& event);
+	};
+
+}
+
+#endif // !GUI_MOUSE_BUTTON_PRESSED_BEHAVIOR_H
---
+++ src/GUI/MouseButtonPressedBehavior.cpp
@@ -0,0 +1,26 @@
+#include "GUI/MouseButtonPressedBehavior.h"
+#include "GUI/SignalTypes.h"
+
+namespace gui {
+	gui::MouseButtonPressedBehavior::MouseButtonPressedBehavior(std::shared_ptr<gui::UIObject> guiObject)
+	{
+		this->guiObject = guiObject;
+		this->eventType = sf::Event::MouseButtonPressed;
+	}
+
+	void gui::MouseButtonPressedBehavior::OnHandleEvent(const sf::Event& event)
+	{
+		if (event.type == eventType) {
+
+			switch (event.mouseButton.button)
+			{
+			case sf::Mouse::Left:
+				guiObject->GetSignal(SignalTypes::onLeftMouseButtonPressed)();
+				break;
+			case sf::Mouse::Right:
+				guiObject->GetSignal(SignalTypes::onRightMouseButtonPressed)();
+				break;
+			}
+		}
+	}
+}
