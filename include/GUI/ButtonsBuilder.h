6b5e7713d49b17b62844dd3c0c1d765850746753
Bezifabr <bezifabr@gmail.com>
2020-05-14 23:58:48 +0200

    [DPS-56] Implement ButtonsBuilder class

---
+++ include/GUI/ButtonsBuilder.h
@@ -0,0 +1,22 @@
+#ifndef GUI_TEXT_BUTTON_BUILDER_H
+#define GUI_TEXT_BUTTON_BUILDER_H
+
+#include "GUI/TextButton.h"
+
+namespace gui {
+
+	class ButtonsBuilder {
+
+	public:
+		void SetTexture(const sf::Texture& texture);
+		void SetFont(const sf::Font& font);
+		std::shared_ptr<gui::ClickableComponent> BuildClickableRegion(sf::FloatRect region);
+		std::shared_ptr<gui::TextButton> BuildTextButton(const std::string& text, sf::Vector2f position);
+
+	private:
+		const sf::Texture* texture;
+		const sf::Font* font;
+	};
+}
+
+#endif // GUI_TEXT_BUTTON_BUILDER_H
---
+++ src/GUI/ButtonsBuilder.cpp
@@ -0,0 +1,23 @@
+#include "GUI/ButtonsBuilder.h"
+
+void gui::ButtonsBuilder::SetTexture(const sf::Texture& texture)
+{
+	this->texture = &texture;
+}
+
+void gui::ButtonsBuilder::SetFont(const sf::Font& font)
+{
+	this->font = &font;
+}
+
+std::shared_ptr<gui::ClickableComponent> gui::ButtonsBuilder::BuildClickableRegion(sf::FloatRect region)
+{
+	auto clickable = std::shared_ptr<gui::ClickableComponent>(new ClickableComponent);
+	clickable->SetRegion(region);
+	return clickable;
+}
+
+std::shared_ptr<gui::TextButton> gui::ButtonsBuilder::BuildTextButton(const std::string& text, sf::Vector2f position)
+{
+	return std::shared_ptr<gui::TextButton>(new gui::TextButton (*texture, *font, text, position));
+}
