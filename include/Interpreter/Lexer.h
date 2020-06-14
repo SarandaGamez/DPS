f1d3af022ac5e134e2549ed2ff10dd8e0fffab3c
Bezifabr <bezifabr@gmail.com>
2020-06-14 22:43:47 +0200

    [DPS-164] Implement Lexer for scripting language

---
+++ include/Interpreter/Lexer.h
@@ -0,0 +1,16 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <string>
+#include <queue>
+
+namespace scripts {
+	class Lexer {
+	public:
+		std::queue<std::string> FindPhrases(const std::string& line);
+	private:
+		bool IsSpecialCharacter(const char& character) const;
+	};
+}
+
+#endif // !LEXER_H
---
+++ src/Interpreter/Lexer.cpp
@@ -0,0 +1,46 @@
+#include "Interpreter/Lexer.h"
+
+namespace scripts {
+	std::queue<std::string> Lexer::FindPhrases(const std::string& line)
+	{
+		std::string phrase;
+		std::queue<std::string> phrasesQueue;
+
+		for (auto character : line)
+		{
+			if (phrase == "//")
+				return phrasesQueue;
+
+			if (IsSpecialCharacter(character))
+			{
+				if (!phrase.empty())
+				{
+					phrasesQueue.push(phrase);
+					phrase.clear();
+				}
+
+				if (character != '	' && character != ' ')
+				{
+					phrase.push_back(character);
+					phrasesQueue.push(phrase);
+					phrase.clear();
+				}
+			}
+			else
+			{
+				phrase.push_back(character);
+			}
+		}
+
+		if (!phrase.empty())
+			phrasesQueue.push(phrase);
+
+		return phrasesQueue;
+	}
+
+	bool Lexer::IsSpecialCharacter(const char& character) const
+	{
+		std::vector<char> charactersArray = { ' ', '\t', '{', '}', '(', ')', '"', ';', ',', '.', '=' };
+		return std::any_of(charactersArray.begin(), charactersArray.end(), [character](char c) { return character == c; });
+	}
+}
