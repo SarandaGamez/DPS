37d9a0e9800ef6e7441900a91217b1167c9fc661
Bezifabr <bezifabr@gmail.com>
2020-07-07 16:27:49 +0200

    [DPS-214] Remove GUI components management from GameState

--- include/States/GameState.h
+++ include/States/GameState.h
@@ -5,21 +5,13 @@
 #include <SFML/Audio.hpp>
 #include "GUI/ClickableComponent.h"
 #include "GUI/UIComposite.h"
-#include "GUI/UICompositeBuilder.h"
-#include "GUI/Window.h"
-#include "GUI/Notebook.h"
 #include "Controllers/Controllers.h"
 #include <vector>
 #include <memory>
 
 class GameState : public State {
-	sf::Sprite background;
 	sf::Font font;
-	std::vector<std::shared_ptr<gui::UIComposite>> components;
 	ctrl::Controllers controllers;
-	gui::UICompositeBuilder compositeBuilder;
-	std::shared_ptr<gui::Window> dialogWindow;
-	unsigned short currentLayer = 0;
 private:
 
 	virtual void OnHandleEvent(sf::Event event) override;
@@ -29,10 +21,6 @@ private:
 
 	virtual void OnEnter() override;
 	virtual void OnLeave() override;
-
-	void OpenDialog();
-	void CloseDialog();
-	void AddUIComponent(std::shared_ptr<gui::UIComponent> component);
 };
 
 #endif // !GAME_STATE_H
--- src/States/GameState.cpp
+++ src/States/GameState.cpp
@@ -1,8 +1,6 @@
 #include "States/GameState.h"
 #include "States/OverState.h"
 #include "StatesMachine.h"
-#include "GUI/DialogWindowBuilder.h"
-#include "GUI/ButtonsBuilder.h"
 #include "GUI/ComponentsBuilder.h"
 #include "Controllers/NotebookController.h"
 #include "Controllers/DebugController.h"
@@ -36,34 +34,6 @@ void GameState::OnEnter()
 
 	ctrl::Controller::Initialize(renderWindow, textures, transition);
 	gui::ComponentsBuilder::SetFont(&font);
-	gui::ButtonsBuilder buttonsBuilder;
-
-	auto clickable = buttonsBuilder.BuildClickableRegion({ 1230, 790, 190, 90});
-	clickable->GetSignal(gui::SignalTypes::onLeftMouseButtonReleased).Connect(std::bind(&GameState::OpenDialog, this));
-	AddUIComponent(clickable);
-
-	clickable = buttonsBuilder.BuildClickableRegion({ 1525, 800, 100, 90 });
-	clickable->GetSignal(gui::SignalTypes::onLeftMouseButtonReleased).Connect(std::bind(&GameState::OpenDialog, this));
-	AddUIComponent(clickable);
-
-	clickable = buttonsBuilder.BuildClickableRegion({ 1715, 1030, 200, 50 });
-	clickable->GetSignal(gui::SignalTypes::onLeftMouseButtonReleased).Connect(std::bind(&GameState::OpenDialog, this));
-	AddUIComponent(clickable);
-
-
-	background.setTexture(textures.GetTexture("OfficeBackground"));
-
-	auto windowBgr = std::shared_ptr<gui::GraphicComponent>(new gui::GraphicComponent(textures.GetTexture("Window")));
-
-	gui::DialogWindowBuilder dialogWindowBuilder(std::shared_ptr<gui::GraphicComponent>(new gui::GraphicComponent(textures.GetTexture("DialogWindow"))));
-	dialogWindowBuilder.SetCaption("Feature is not available at the moment", font);
-	dialogWindowBuilder.AddOption("Close", font, textures.GetTexture("Button"))
-		->GetSignal(gui::SignalTypes::onLeftMouseButtonReleased).Connect(std::bind(&GameState::CloseDialog, this));
-	dialogWindow = dialogWindowBuilder.GetResult();
-	dialogWindow->SetPosition({renderWindow->getSize().x / 2 - dialogWindow->GetWindowArea().width / 2, renderWindow->getSize().y / 2 - dialogWindow->GetWindowArea().height / 2 });
-	dialogWindow->SetLayer(1);
-	components.push_back(dialogWindow);
-	CloseDialog();
 
 	// Controllers
 	controllers.Add(new ctrl::LocationController);
@@ -85,55 +55,17 @@ void GameState::OnLeave()
 	cout << "Game unloaded" << endl;
 }
 
-void GameState::OpenDialog()
-{
-	cout << "TEST" << endl;
-	dialogWindow->SetVisible(true);
-	dialogWindow->SetActive(true);
-	this->currentLayer = 1;
-}
-
-void GameState::CloseDialog()
-{
-	cout << "TEST2" << endl;
-	dialogWindow->SetVisible(false);
-	dialogWindow->SetActive(false);
-	this->currentLayer = 0;
-}
-
-void GameState::AddUIComponent(std::shared_ptr<gui::UIComponent> component)
-{
-	compositeBuilder.Reset();
-	compositeBuilder.Add(component);
-	components.push_back(compositeBuilder.GetResult());
-}
-
 void GameState::OnUpdate(sf::Time elapsedTimes)
 {
-	if(!components.empty())
-	for (auto &comp : components)
-		if(comp->GetLayer() == currentLayer)
-			comp->Update(elapsedTimes);
 	controllers.Update(elapsedTimes);
 }
 
 void GameState::OnHandleEvent(sf::Event event)
 {
-	if (!components.empty())
-	for (auto &comp : components)
-		if (comp->GetLayer() == currentLayer)
-			comp->HandleEvent(event);
 	controllers.HandleEvent(event);
 }
 
 void GameState::OnDraw()
 {
-	renderWindow->draw(background);
-
-	if (!components.empty())
-	for (auto &comp : components)
-		if (comp->GetLayer() == currentLayer)
-			renderWindow->draw(*comp);
-
 	controllers.Draw();
 }
